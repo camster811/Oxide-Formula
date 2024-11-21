754ce685ea5c106d578d9dc586d7bbb7793029b8
Camster811 <cls0150@auburn.edu>
2024-11-21 02:40:56 -0600

    add case for map reducers

--- routes/retrieve.py
+++ routes/retrieve.py
@@ -5,11 +5,20 @@ from oxide.core import oxide as oxide
 retrieve_router = APIRouter()
 
 @retrieve_router.get("/retrieve")
-async def retrieve_module(selected_module: str, selected_collection: str):
+async def retrieve_module(selected_module: str, selected_collection: str = None, selected_oid: str = None):
     try:
-        cid = oxide.get_cid_from_name(selected_collection)
-        files = oxide.expand_oids(cid)
-        results = oxide.retrieve(selected_module, files)
-        return results
+        if selected_oid:
+            oid_list = []
+            oid_list.append(selected_oid)
+            
+            results = oxide.retrieve(selected_module, oid_list)
+            return results
+        
+        if selected_collection:
+            cid = oxide.get_cid_from_name(selected_collection)
+            files = oxide.expand_oids(cid)
+            results = oxide.retrieve(selected_module, files)
+            return results
+        
     except Exception as e:
         return {"error": str(e)}
